951180da1efc9fbcebcf8166f9aa4b7813b28d49
gsganden <gsganden@gmail.com>
2025-03-03 20:03:44 +0000

    Commit weird file in case we need it

---
+++ llmfoundry/loggers/file_logger_wrapper.py
@@ -0,0 +1,8 @@
+# Copyright 2024 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Wrapper for Composer's FileLogger to register it in llm-foundry."""
+
+from composer.loggers.file_logger import FileLogger
+
+__all__ = ['FileLogger'] 
