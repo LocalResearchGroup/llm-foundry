a2c9c85a0e64a4dfb98e7bd42042cf83f6edfa54
daol <506234+tyoc213@users.noreply.github.com>
2025-03-13 20:37:49 -0600

    Refactor to single file and multiple calls for datasets: finemath, tulu and numina

--- scripts/data_prep/split_hf_datasets.py
+++ scripts/data_prep/split_hf_datasets.py
@@ -1,74 +1,99 @@
+from argparse import ArgumentParser, Namespace
+
 from datasets import load_dataset, load_from_disk, DatasetDict
 from huggingface_hub import HfApi, login
 from pathlib import Path
 import os
 
-
-def split_dataset(
-    inp_ds_path: str,
-    inp_ds_name: str | None = None,
-    out_ds_path: str | None = None,
-    test_size: float = 0.1,
-    seed: int = 42,
-    shard_size: str = "300MB",
-):
-    """Split a dataset into train and test sets and save to disk"""
-    if not os.environ.get("HUGGING_FACE_HUB_TOKEN"):
-        print("No Hugging Face token found. Please login.")
-        login()
-    
-    print(f"Loading dataset {inp_ds_path}/{inp_ds_name}")
-    dataset = load_dataset(path=inp_ds_path, name=inp_ds_name, split="train")
-
-    print(f"Splitting dataset")
-    split_dataset = dataset.train_test_split(test_size=test_size, seed=seed)
-
-    print(f"Saving dataset to {out_ds_path}")
-    split_dataset.save_to_disk(out_ds_path)
-
-    combined = DatasetDict({"train": split_dataset["train"], "valid": split_dataset["test"]})
-
-    print(f"Uploading dataset to {out_ds_path}")
-    combined.push_to_hub(out_ds_path, config_name="full", private=True, max_shard_size=shard_size)
-
-    # save splits locally
+def save_to_parquet(combined: DatasetDict, out_ds_path: Path):
+    data_files = dict()
     for split, dataset in combined.items():
-        filename = f"dataset-{split}.parquet"
+        filename = out_ds_path /f"{split}.parquet"
+        data_files[split] = filename
         if not Path(filename).exists():
-            print(f"Saving dataset-{split}.parquet")
+            print(f"Saving {filename}")
             dataset.to_parquet(filename)
         else:
-            print(f"dataset-{split}.parquet exist. Skipping...")
-
-    # load from local
-    data_files = {"train": "dataset-train.parquet", "valid": "dataset-valid.parquet"}
-
-    print("Loading parquet training/val")
-    raw_datasets = load_dataset("parquet", data_dir=".", data_files=data_files)
-
+            print(f"{filename} already exist. Skipping...")
+    return data_files
 
 def create_size_ablation(dataset, total_rows):
     """Create a subset with a given percentage of the original data"""
     train_size = int(total_rows * 0.9)
     return {
         "train": dataset["train"].shuffle(42).select(range(train_size)),
-        "valid": dataset["valid"].shuffle(42).select(range(total_rows - train_size)),
+        "test": dataset["test"].shuffle(42).select(range(total_rows - train_size)),
     }
 
 
-a = [1_000_000, 100_000, 10_000, 1000]
-l = ["1M", "100k", "10k", "1k"]
+def push_ablations(raw_datasets, ablations, hf_repo, config_name, private, shard_size):
+    print(f"creating ablations from {len(raw_datasets['train'])}/{len(raw_datasets['test'])}")
+    for label in ablations:
+        match label[-1]:
+            case "M":
+                ds = create_size_ablation(raw_datasets, int(label[:-1]) * 1_000_000)
+            case "k":
+                ds = create_size_ablation(raw_datasets, int(label[:-1]) * 1_000)
+            case _:
+                ds = raw_datasets
+    
+        dsdict = DatasetDict(
+            {
+                "train": ds["train"],
+                "test": ds["test"],
+            }
+        )
+    
+        print(f"Uploading ablation {label} train/val")
+        
+        dsdict.push_to_hub(hf_repo, config_name=label, private=private, max_shard_size=shard_size)
 
-for amount, label in zip(a, l):
-    print(f"Creating ablation {label} train/val")
-    ds = create_size_ablation(raw_datasets, amount)
+def pull_n_push(
+    hf_ds_tgt,
+    hf_ds_src,
+    ds_name=None,
+    after_pull=None,
+    test_size: float = 0.1,
+    seed: int = 42,
+    saving2parquet=False,
+    ablations = ("full", "1M", "100k", "10k", "1k"),
+    private=True,
+    shard_size: str = "300MB",
+    purge_cache=False,
+):
+    banner = f"Loading dataset {hf_ds_src}/{'default' if ds_name is None else ds_name}"
+    print("#"*len(banner))
+    print(banner)
+    print("#"*len(banner))
+    dataset = load_dataset(path=hf_ds_src, name=ds_name, split="train")
+    if after_pull is not None:
+        dataset = after_pull(dataset)
+    dataset = dataset.train_test_split(test_size=test_size, seed=seed)
+    dsd = DatasetDict({"train": dataset["train"], "test": dataset["test"]})
 
-    dsdict = DatasetDict(
-        {
-            "train": ds["train"],
-            "valid": ds["valid"],
-        }
-    )
+    if saving2parquet:
+        print(f"Saving parquet to {hf_ds_tgt} train/test")
+        out_ds_path = Path(hf_ds_tgt)
+        out_ds_path.mkdir(parents=True, exist_ok=True)
+        data_files = save_to_parquet(dsd, out_ds_path.absolute())
+        # print(f"Loading parquet training/val from {str(out_ds_path)}\n\n\n")
+        # dsd = load_dataset(str(out_ds_path))
 
-    print(f"Uploading ablation {label} train/val")
-    dsdict.push_to_hub(hf_dataset, config_name=label, private=False)
+    push_ablations(dsd, ablations, hf_ds_tgt, ds_name, private, shard_size)
+
+    if purge_cache:
+       dataset.cleanup_cache_files()
+
+def filter_tulu(dataset):
+    print(f"Original dataset rows {len(dataset)}")
+    dataset = dataset.filter(lambda r: r["source"] is not None and "aya" not in r["source"])
+    print(f"         current rows {len(dataset)}")
+    return dataset
+
+if __name__ == "__main__":
+    if not os.environ.get("HUGGING_FACE_HUB_TOKEN"):
+        print("No Hugging Face token found. Please login.")
+        login()
+    pull_n_push("LocalResearchGroup/split-finemath", "HuggingFaceTB/finemath", "finemath-4plus")
+    pull_n_push("LocalResearchGroup/split-tulu-3-sft-olmo-2-mixture", "allenai/tulu-3-sft-olmo-2-mixture", after_pull=filter_tulu, ablations=("full", "100k", "10k", "1k"))
+    pull_n_push("LocalResearchGroup/split-NuminaMath-CoT", "AI-MO/NuminaMath-CoT", ablations=("full", "100k", "10k", "1k"))
