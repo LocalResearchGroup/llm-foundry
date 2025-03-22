5ef92f9eb008b9504e9e74d2841704cfe6291bc1
daol <506234+tyoc213@users.noreply.github.com>
2025-03-21 22:57:56 -0600

    OK Splitting into train/test and starting exploring dataset tokenization
    Tokenization works OK with finemath as it already has the `text` column,
    we can:
    
    * post process when downloading the dataset in `split_hf_dataset.py` or
    * we can call convert_finetuning_dataset and create the preprocesors at
    `data_prep/preproc` and start from there (this is a WIP)

---
+++ scripts/data_prep/preproc/preprocs.py
@@ -0,0 +1,30 @@
+from llmfoundry.data.finetuning.tasks import (
+    DatasetConstructor
+)
+
+dataset_constructor = DatasetConstructor()
+
+@dataset_constructor.register("tyoc213/split-finemath")
+def preprocessing_function(inp: dict) -> dict:
+    """Format the already-split example."""
+    return {
+        'prompt': inp['inputs'] + ':',
+        'response': inp['targets'],
+    }
+
+@dataset_constructor.register("tyoc213/split-tulu-3-sft-olmo-2-mixture")
+def pre_tulu(inp: dict):
+    pro = [m["content"] for m in inp["messages"] if m["role"] == "user"]
+    ans = [m["content"] for m in inp["messages"] if m["role"] == "assistant"]
+
+    prompt = "\n".join(pro)
+    answer = "\n".join(ans)
+    if len(prompt) < len(answer):
+        # FIXME: padding needed if not we get an error?
+        prompt = prompt + "  " + " " * (len(answer) - len(prompt)) 
+    return {'prompt': prompt, 'response': answer}
+
+
+@dataset_constructor.register("tyoc213/split-NuminaMath-CoT")
+def pre_numina(inp: dict):
+    return {'prompt': inp['problem'], 'response': inp['solution']}
