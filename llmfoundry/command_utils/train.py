1eacc97c302d3b186b187592034c83c622223c1d
Vishal Bakshi <vdbakshi@gmail.com>
2025-03-28 11:14:32 -0700

    Update train.py

--- llmfoundry/command_utils/train.py
+++ llmfoundry/command_utils/train.py
@@ -554,7 +554,6 @@ def train(cfg: DictConfig) -> Trainer:
     compile_config = train_cfg.compile_config
 
     # added by Vishal: print out model weights' dtype
-    print(f"model.dtype:{model.dtype}")
     print(f"q_proj base layer dtype:{model.model.base_model.model.model.layers[0].self_attn.q_proj.base_layer.weight.dtype}")
     print(f"q_proj lora_A dtype:{model.model.base_model.model.model.layers[0].self_attn.q_proj.lora_A.default.weight.dtype}")
     print(f"q_proj lora_B dtype:{model.model.base_model.model.model.layers[0].self_attn.q_proj.lora_B.default.weight.dtype}")
