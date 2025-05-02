9dca0bf3f1ca4419088e0d0dddf519ceb2c71578
daol <506234+tyoc213@users.noreply.github.com>
2025-05-02 14:45:41 -0600

    tokenize each row and padd it up to max_length

--- llmfoundry/command_utils/data_prep/convert_dataset_hf.py
+++ llmfoundry/command_utils/data_prep/convert_dataset_hf.py
@@ -338,7 +338,7 @@ def convert_dataset_hf(
     try:
         if "tulu" not in CONSTS:
             import dataset_constants_split_config
-            your_user = ...
+            your_user = "tyoc213"
             dataset_constants_split_config.register_new_datasets(your_user)
         dataset_constants = CONSTS[dataset]
     except KeyError:
