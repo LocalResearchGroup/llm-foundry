c97cdafa23aa1e25b62ac63ca12f15fbeddb3fd4
galopy <shipalnomoo@gmail.com>
2025-08-02 18:42:47 -0500

    Register a new custom model

--- llmfoundry/models/llama/register.py
+++ llmfoundry/models/llama/register.py
@@ -1,15 +1,13 @@
 """Registration utilities for Llama models."""
 
-from llmfoundry.models.llama.model import CustomLlamaModel
-
+from llmfoundry.models.llama.custom_model import CustomLlamaModel
 
 def get_custom_llama_model():
     """Get the CustomLlamaModel class."""
     return CustomLlamaModel
 
-
 def register_custom_llama_model():
     """Register the custom Llama model with the registry."""
     from llmfoundry import registry
-    registry.models.register("hf_causal_lm")(CustomLlamaModel)
+    registry.models.register("custom_model")(CustomLlamaModel)
     return CustomLlamaModel 
