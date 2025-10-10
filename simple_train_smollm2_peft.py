91575f84ea9f52e57b6227fc89e11f94c30866e4
galopy <shipalnomoo@gmail.com>
2025-10-10 06:50:42 -0500

    Update yaml scripts with sequence packing

--- simple_train_smollm2_peft.py
+++ simple_train_smollm2_peft.py
@@ -13,8 +13,6 @@ from llmfoundry.models.llama.register import register_custom_llama_model
 from llmfoundry.command_utils.train import train
 from omegaconf import OmegaConf
 
-import text_generation_callback  # type: ignore
-
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
