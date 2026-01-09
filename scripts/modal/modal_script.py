2fe5a33d73008fd985159621fe9e3c2956de9d47
tyoc213 <506234+tyoc213@users.noreply.github.com>
2026-01-09 01:59:52 -0600

    text and tokenize dataset in tyoc213 user

--- scripts/modal/modal_script.py
+++ scripts/modal/modal_script.py
@@ -437,7 +437,7 @@ def main():
     import time
     run_ts = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     print(run_ts)
-    preprocess_datasets.remote() if False else None
+    preprocess_datasets.remote() if True else None
     tokenize_datasets.remote() if True else None
 
     get_stats.remote()
